01866c32bf9d3956cd0e226f3e42433172312274
nachodoria <ignaciodoriaoberman@gmail.com>
2022-12-12 22:28:45 -0300

    Started

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import AnimatedTitle from './assets/animatedTexts/animeatedTitle';
 
 function App() {
   return (
-  <></>
+
+    <div className='main-container'>
+      <AnimatedTitle className="main-title" text={"JAPAN"} style={{ fontFamily: "Japan" }} />
+    </div>
+
   );
 }
 
---
+++ src/assets/animatedTexts/animeatedTitle.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+import "../../App.css"
+
+const AnimatedTitle = ({text}) => {
+    const words = text.split("");
+
+    const container = {
+        hidden: { opacity: 0 },
+        visible: (i = 1) => ({
+            opacity: 1,
+            transition: { staggerChildren: 0.14, delayChildren: 0.04 * i },
+        }),
+    };
+
+    const child = {
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                type: "spring",
+                damping: 18,
+                stiffness: 100,
+            },
+        },
+        hidden: {
+            opacity: 0,
+            y: 30,
+            transition: {
+                type: "spring",
+                damping: 18,
+                stiffness: 100,
+            },
+        },
+    };
+
+    return (
+        <motion.div
+            style={{ overflow: "hidden", display: "flex"}}
+            variants={container}
+            initial="hidden"
+            animate="visible"
+        >
+            {words.map((word, index) => (
+                <motion.h1
+                    variants={child}
+                    key={index}
+                    className="japan-title"
+                    style={{color: index == 1 ? "#BC012C" : "white"}}
+                >
+                    {word}
+                </motion.h1>
+            ))}
+        </motion.div>
+    )
+}
+
+
+export default AnimatedTitle;
